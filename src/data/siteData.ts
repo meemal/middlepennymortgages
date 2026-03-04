1d5ad6693c4336c0898a6aa5ce4caeeff8124ffd
Naomi Spirit <naomispirit@gmail.com>
2026-03-04 10:05:05 +0000

    Updated siteData.ts

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -18,6 +18,6 @@ export const siteData = {
   },
   fee: '£395',
   valueStatement: `At Middlepenny, we place our clients at the heart of everything we do. Our business is built on trust, integrity and lasting relationships, with a commitment to delivering clear, reliable guidance at every stage of the journey. We believe in removing complexity, providing reassurance, and ensuring our clients feel fully supported in their decisions. By combining professionalism with a personal approach, our aim is to become a trusted partner you can return to time and again, confident that your best interests are always our priority.`,
-  regulatoryDisclosure: `Middle Penny Mortgage Solutions is a trading name of Emma Murphy who is an appointed representative of HL Partnership Limited which is authorised and regulated by the Financial Conduct Authority.`,
+  regulatoryDisclosure: `Middlepenny is a trading name of Emma Murphy who is an appointed representative of HL Partnership Limited which is authorised and regulated by the Financial Conduct Authority.`,
   jurisdictionDisclosure: `The guidance and/or information contained within this website is subject to the UK regulatory regime and is therefore targeted at consumers based in the UK.`,
 };
